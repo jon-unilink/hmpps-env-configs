15870a31c7de89679c166362ad9d7e7579762796
Chris Kinsella <chris.kinsella@helkinsoft.com>
2020-03-10 11:09:25 +0000

    updated psn deployment scripts for stage'

--- delius-stage/psn-proxy/scripts/deploy_scripts_to_psn_proxy.sh
+++ delius-stage/psn-proxy/scripts/deploy_scripts_to_psn_proxy.sh
@@ -4,7 +4,15 @@ chmod u+x *.sh
 scp *.sh ec2-user@hmpps-delius-psn-test-a:/opt/hmpps/proxy-scripts/
 scp *.sh ec2-user@hmpps-delius-psn-test-b:/opt/hmpps/proxy-scripts/
 
+
+scp ../im-proxy.conf ec2-user@hmpps-delius-psn-test-a:/opt/hmpps/proxy-scripts/im-proxy-stage.conf
+scp ../im-proxy.conf ec2-user@hmpps-delius-psn-test-b:/opt/hmpps/proxy-scripts/im-proxy-stage.conf
+
 # manually download tls certs from AWS S3 & navigate to the download folder
 cd ~/Downloads
+
 scp -r *.pem ec2-user@hmpps-delius-psn-test-a:/opt/hmpps/proxy-scripts
+scp -r *.key ec2-user@hmpps-delius-psn-test-a:/opt/hmpps/proxy-scripts
+
+scp -r *.pem ec2-user@hmpps-delius-psn-test-b:/opt/hmpps/proxy-scripts
 scp -r *.key ec2-user@hmpps-delius-psn-test-b:/opt/hmpps/proxy-scripts
